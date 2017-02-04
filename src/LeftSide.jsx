c4c01f02c652e2ef5b103bdc0fd432ae522c6389
yangdefeng <yangdefeng@weixinhai.com>
2017-02-04 17:57:23 +0800

    checkbox & empty

--- src/LeftSide.jsx
+++ src/LeftSide.jsx
@@ -1,4 +1,5 @@
 import React, {Component, PropTypes} from 'react';
+const update = require('react-addons-update');
 
 let recruitItems = [
 	{department:'工程研发部门',total:120,children:[
@@ -16,15 +17,21 @@ class RecruitJob extends Component {
 	constructor(props) {
 		super(props);
 	}
+	handleClick = (i)=> {
+		let arr = update(this.props.selected, {[i]: {$apply:x=>!x}});
+		this.props.updateState(this.props.index,arr);
+	};
 	render() {
 		return (
 			<ul>
 				{
 					this.props.jobLists.map((ele,i)=>(
 						<li key={i}>
-							<input type="checkbox" />
-							<label>{ele.position}</label>
-							<span>{ele.num}</span>
+							<i className="checkboxFont" onClick={()=>this.handleClick(i)}>
+								{this.props.selected&&this.props.selected[i]?'√':''}
+							</i>
+							<span className="name">{ele.position}</span>
+							<span className="num">{ele.num}</span>
 						</li>
 					))
 				}
@@ -37,21 +44,37 @@ class Department extends Component {
 	constructor(props) {
 		super(props);
 	}
+	handleClick = (index)=> {
+		let arr = this.props.checked[index];
+		let allChecked = arr&&arr.every(x=>x);
+		for (let i=0; i<arr.length; i++) {
+			arr[i] = !allChecked;
+		}
+		this.props.updateState(index,arr);
+	};
 	render() {
 		return (
 			<div>
 				{
-					this.props.items.map((item,index)=>(
-						<div key={index} className="sidebarContent">
-							<p>
-								<input type="checkbox" />
-								<label>{item.department}</label>
-								<i className="triangleDown" />
-								<span>{item.total}</span>
-							</p>
-							<RecruitJob jobLists={item.children} />
-						</div>
-					))
+					this.props.items.map((item,index)=>{
+						let childArrs = this.props.checked[index];
+						return (
+							<div key={index} className="sidebarContent">
+								<p>
+									<i className="checkboxFont" onClick={()=>this.handleClick(index)}>
+										{childArrs.length>0 && childArrs.every(x=>x) ? '√' : ''}
+									</i>
+									<span className="name">{item.department}</span>
+									<span className="num">{item.total}</span>
+								</p>
+								<RecruitJob jobLists={item.children}
+								            index={index}
+								            selected={childArrs}
+								            updateState={this.props.updateState}
+								/>
+							</div>
+						)
+					})
 				}
 			</div>
 		);
@@ -61,12 +84,36 @@ class Department extends Component {
 export class LeftSide extends Component {
 	constructor(props) {
 		super(props);
+		let checked = [];
+		for (let i=0; i<recruitItems.length; i++) {
+			let length = recruitItems[i].children.length;
+			let arr = [];
+			for (let j=0; j<length; j++) {
+				arr[j] = false;
+			}
+			checked.push(arr);
+		}
+		this.state = {checked:checked};
 	}
+	updateState = (index,arr)=> {
+		this.setState(update(this.state, {checked: {[index]: {$apply:x=>arr}}}));
+	};
+	handleEmpty = ()=> {
+		let checked = [];
+		for (let i=0; i<this.state.checked.length; i++) {
+			let arr = this.state.checked[i];
+			for (let j=0; j<arr.length; j++) {
+				arr[j] = false;
+			}
+			checked.push(arr);
+		}
+		this.setState({checked:checked});
+	};
 	render() {
 		return (
 			<div className="sidebar">
-				<p className="sidebarHeader">招聘职位<span>清空</span></p>
-				<Department items={recruitItems} />
+				<p className="sidebarHeader">招聘职位<span onClick={this.handleEmpty}>清空</span></p>
+				<Department items={recruitItems} updateState={this.updateState} checked={this.state.checked}/>
 			</div>
 		);
 	}
